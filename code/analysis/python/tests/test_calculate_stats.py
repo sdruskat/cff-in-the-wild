cba1743845ca03019cdd80eb113d732a09f41a68
Sam Harrison <samharrison.xg@gmail.com>
2022-04-07 14:28:37 +0100

    Add CFF version check

--- code/analysis/python/tests/test_calculate_stats.py
+++ code/analysis/python/tests/test_calculate_stats.py
@@ -1,4 +1,4 @@
-from python.calculate_stats import created_with_cffinit
+from python.calculate_stats import created_with_cffinit, which_cff_version
 """Tests for the calculate_tests.py file"""
 
 
@@ -17,3 +17,14 @@ def test_created_by_cffinit():
         not_created_by_cffinit
     ])
     assert n_cffinit == 1
+
+
+def test_which_cff_version():
+    """Test the function that outputs the number
+    of CFF files with each version"""
+    v111 = { 'cff-version': '1.1.1' }
+    v112 = { 'cff-version': '1.1.2' }
+    cff_data = [v111, v111, v112]
+    cff_version = which_cff_version(cff_data)
+    assert cff_version['1.1.1'] == 2
+    assert cff_version['1.1.2'] == 1
