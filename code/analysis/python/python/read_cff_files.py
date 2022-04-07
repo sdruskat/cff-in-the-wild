05dedc4ef3ffd9b0c6e771e9a412f59208535a81
Stephan Druskat <stephan.druskat@dlr.de>
2022-04-07 14:29:02 +0200

    Add sanity check for CFF metadata

--- code/analysis/python/python/read_cff_files.py
+++ code/analysis/python/python/read_cff_files.py
@@ -4,6 +4,15 @@ import yaml
 import argparse
 
 
+def sanity_check(cff_file: dict) -> bool:
+    """Checks if the required keys for CFF exist in the given file.
+
+    :param cff_file: A dictionary containing the metadata from a CFF file
+    :return: whether the required keys for CFF exist in the file
+    """
+    return all(k in cff_file for k in ('cff-version', 'message', 'title', 'authors'))
+
+
 def read_cff_files(datadir):
     """Read CFF files from a directory and return a list of valid
     YAML CFF data and list of invalid YAML filenames"""
@@ -19,7 +28,10 @@ def read_cff_files(datadir):
                 # invalid file list
                 try:
                     cff_file = yaml.safe_load(f)
-                    cff_data.append(cff_file)
+                    if sanity_check(cff_file):
+                        cff_data.append(cff_file)
+                    else:
+                        invalid_file.append(file)
                 except:
                     invalid_file.append(file)
     return cff_data, invalid_file
