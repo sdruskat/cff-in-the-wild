3b7d2c20299b8a04cfa1026daa993c2266dcea26
Sam Harrison <samharrison.xg@gmail.com>
2022-04-07 13:42:44 +0100

    Add check for CFFinit comment

--- code/analysis/python/python/read_cff_files.py
+++ code/analysis/python/python/read_cff_files.py
@@ -14,11 +14,20 @@ def read_cff_files(datadir):
         # Check it's a .cff file
         if file.endswith('.cff'):
             # Open the file, read the YAML and append to cff_data
-            with open(os.path.join(datadir, file)) as f:
+            with open(os.path.join(datadir, file), encoding='latin-1') as f:
+                # Read the first line, which should contain the comment
+                # saying it was created with CFFinit, if it was
+                cff_str = f.readline()
                 # Try to parse the YAML, and if we can't, add to the
                 # invalid file list
                 try:
                     cff_file = yaml.safe_load(f)
+                    # Was the file created by CFFinit? Relies on this comment
+                    # being on the first line
+                    if 'This CITATION.cff file was generated with cffinit' in cff_str:
+                        cff_file['created_by_cffinit'] = True
+                    else:
+                        cff_file['created_by_cffinit'] = False
                     cff_data.append(cff_file)
                 except:
                     invalid_file.append(file)
@@ -29,4 +38,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--datadir', type=str, required=True, help='Directory in which CFF file are stored')
     args = parser.parse_args()
-    cff_data, invalid_file = read_cff_files(args.datadir)
+
+    # Read the CFF files and print how many are valid YAML
+    cff_data, invalid_files = read_cff_files(args.datadir)
+    print(f'CFF files: {len(cff_data)}')
+    print(f'Invalid files: {len(invalid_files)}')
+
+    # Check how many were created using CFFinit
+    cffinit = 0
+    for cff in cff_data:
+        if cff['created_by_cffinit']:
+            cffinit = cffinit + 1
+    print(f'Files created with CFFinit: {cffinit}')
